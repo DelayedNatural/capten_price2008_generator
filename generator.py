345f6de72ba3a7fec11f7f413f501b60011389e8
petros <petrosnigault@gmail.com>
2025-07-15 17:49:25 +0300

    save

--- generator.py
+++ generator.py
@@ -1,7 +1,30 @@
 import time
+import msvcrt
 dots = ["...", "..", ".", ""]
-n = 5
+n = 3
 for i in range(n):
     for anim in dots:
-        print(f"Initializing generator, please wait{anim}", end="\r", flush=True)
+        print(f"Initializing generator{anim}")
         time.sleep(0.5)
+        print("\n" * 30)
+print("Initialization Complete!")
+for i in range(n):
+    for anim in dots:
+        print("Initialization Complete!")
+        print(f"Parsing configuration files{anim}")
+        time.sleep(0.5)
+        print("\n" * 30)
+print("Parsing complete!")
+for i in range(n):
+    for anim in dots:
+        print("Initialization Complete!")
+        print("Parsing complete!")
+        print(f"Compiling runtime environment{anim}")
+        time.sleep(0.5)
+        print("\n" * 30)
+print("Initialization Complete!")
+print("Parsing complete!")
+print("Runtime environment compiled!")
+option = input('Program is ready to execute. Type your option and press enter to confirm. Type "Y" to start, "N" to abort.')
+if option == "N":
+    msvcrt.getch()
